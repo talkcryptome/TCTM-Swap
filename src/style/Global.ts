770780c51a4844d92dd18595146894150350ce5a
talkcryptome <talkcryptowithme@gmail.com>
2022-03-02 20:34:07 -0700

    Removed unused public folders

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -4,11 +4,6 @@ const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.colors.background};
 
-    @font-face {
-      font-family: 'taco';
-      src: url(/fonts/tacos.ttf);
-    }
-
     img {
       height: auto;
       max-width: 100%;
